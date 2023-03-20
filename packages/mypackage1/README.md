299944389d6af4a99f0b235281d72f3141b5b448
Peter <peter@curlybyte.com>
2023-03-20 21:08:42 +0100

    Prepare package

---
+++ packages/mypackage1/README.md
@@ -0,0 +1,4 @@
+# A Simple package
+
+Install editable:
+`pip install -e .`
---
+++ packages/mypackage1/mypackage1/sub1/extra.py
@@ -0,0 +1,11 @@
+def calc_1(a,b):
+    """Calculate with extra 1
+
+    Args:
+        a (_type_): _description_
+        b (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    return a+b+1
